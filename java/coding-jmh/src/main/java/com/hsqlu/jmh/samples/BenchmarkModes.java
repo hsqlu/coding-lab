2c626a3b52b3984a1d86dc28d2237348e9a255d2
Qiannan Lu <lvqiannan@gmail.com>
2018-12-12 11:08:36 +1300

    Added jmh demo project and samples

--- java/coding-jmh/src/main/java/com/hsqlu/jmh/samples/BenchmarkModes.java
+++ java/coding-jmh/src/main/java/com/hsqlu/jmh/samples/BenchmarkModes.java
@@ -28,8 +28,7 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF
  * THE POSSIBILITY OF SUCH DAMAGE.
  */
-
-package org.sample;
+package com.hsqlu.jmh.samples;
 
 import org.openjdk.jmh.annotations.Benchmark;
 import org.openjdk.jmh.annotations.BenchmarkMode;
@@ -39,31 +38,40 @@ import org.openjdk.jmh.runner.Runner;
 import org.openjdk.jmh.runner.RunnerException;
 import org.openjdk.jmh.runner.options.Options;
 import org.openjdk.jmh.runner.options.OptionsBuilder;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import java.util.concurrent.TimeUnit;
 
-/**
- *
- *
- * Samples ： https://hg.openjdk.java.net/code-tools/jmh/file/tip/jmh-samples/src/main/java/org/openjdk/jmh/samples/
- */
-public class MyBenchmark {
-    private static Logger logger = LoggerFactory.getLogger(MyBenchmark.class);
-    @Benchmark
-    public void testMethod() {
-        // This is a demo/sample template for building your JMH benchmarks. Edit as needed.
-        // Put your benchmark code here.
+public class BenchmarkModes {
 
-        String x = "", y = "", z = "";
+    /*
+     * JMH generates lots of synthetic code for the benchmarks for you during
+     * the benchmark compilation. JMH can measure the benchmark methods in lots
+     * of modes. Users may select the default benchmark mode with a special
+     * annotation, or select/override the mode via the runtime options.
+     *
+     * With this scenario, we start to measure something useful. Note that our
+     * payload code potentially throws exceptions, and we can just declare them
+     * to be thrown. If the code throws the actual exception, the benchmark
+     * execution will stop with an error.
+     *
+     * When you are puzzled with some particular behavior, it usually helps to
+     * look into the generated code. You might see the code is doing not
+     * something you intend it to do. Good experiments always follow up on the
+     * experimental setup, and cross-checking the generated code is an important
+     * part of that follow up.
+     *
+     * The generated code for this particular sample is somewhere at
+     * target/generated-sources/annotations/.../JMHSample_02_BenchmarkModes.java
+     */
 
-        for (int i = 0; i < 100; i++) {
-            x += i; y += i; z += i;
-
-            logger.debug("Concatenating strings " + x + y + z);
-        }
-    }
+    /*
+     * Mode.Throughput, as stated in its Javadoc, measures the raw throughput by
+     * continuously calling the benchmark method in a time-bound iteration, and
+     * counting how many times we executed the method.
+     *
+     * We are using the special annotation to select the units to measure in,
+     * although you can use the default.
+     */
 
     @Benchmark
     @BenchmarkMode(Mode.Throughput)
@@ -163,10 +171,11 @@ public class MyBenchmark {
 
     public static void main(String[] args) throws RunnerException {
         Options opt = new OptionsBuilder()
-                .include(MyBenchmark.class.getSimpleName())
+                .include(BenchmarkModes.class.getSimpleName())
                 .forks(1)
                 .build();
 
         new Runner(opt).run();
     }
+
 }
