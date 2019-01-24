ee4c92ff3c91548067a3f1636615366e1d218fb0
Qiannan Lu <lvqiannan@gmail.com>
2019-01-25 09:39:22 +1300

    Demo of Vertx

---
+++ java/coding-vert.x/src/main/java/com/hsqlu/coding/vertx/MyFirstVerticle.java
@@ -0,0 +1,13 @@
+package com.hsqlu.coding.vertx;
+
+import io.vertx.core.AbstractVerticle;
+
+public class MyFirstVerticle extends AbstractVerticle {
+    public void start() {
+        vertx.createHttpServer().requestHandler(req -> {
+            req.response()
+                    .putHeader("content-type", "text/plain")
+                    .end("Hello World!");
+        }).listen(8080);
+    }
+}
